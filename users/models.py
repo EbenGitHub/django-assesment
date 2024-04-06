6706182b7cb6bfc81b584c69da59362bbf555f95
eenrics <ebenezeresh525@gmail.com>
2024-04-06 13:13:14 +0300

    feat: add user route and model using rest_frameword generic

---
+++ users/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.username
+    
+# class Role(models.Model):
+#     name = models.CharField(max_length=255)
+#     permissions = models.JSONField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     deleted_at = models.DateTimeField(null=True, blank=True)
+    
+#     def __str__(self):
+#         return self.name
+    
+# class Permission(models.Model):
+#     name = models.CharField(max_length=255)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     deleted_at = models.DateTimeField(null=True, blank=True)
+    
+#     def __str__(self):
+#         return self.name
